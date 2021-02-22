d69871d5ddabcb47f3aef0fe0eff2c0443f833c6
tymmaS <190103117@stu.sdu.edu.kz>
2021-02-22 19:26:02 +0600

    initial commit

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<section id="contacts">
+          <div class="container">
+               <div class="section_title">
+                    Contacts
+               </div>
+               <div class="contacts_form">
+                    <form action="">
+                         <input type="text" name="" placeholder="Your name">
+                         <input type="text" name="" placeholder="Number">
+                         <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
+                         <button type="submit">Send</button>
+                    </form>
+               </div>
+          </div>
+     </section>
+
+     <footer>
+          <p>Copyright, 2020</p>
+     </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/myportfolio', function () {
+Route::get('/main', function () {
     return view('main');
 });
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
