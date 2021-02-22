11d8d9c78456afc3115db1728baa43afaccb690f
tymmaS <190103117@stu.sdu.edu.kz>
2021-02-22 19:40:26 +0600

    new routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/Route::get('/portfolio', function () {
+    return view('info');
+});
+Route::get('/aboutme', function () {
+    return view('about');
+});
 
 Route::get('/main', function () {
     return view('main');
@@ -19,4 +24,5 @@ Route::get('/main', function () {
 Route::get('/contacts', function () {
     return view('contacts');
 });
+
 
